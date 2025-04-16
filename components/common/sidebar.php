ca32b677b0f1ecdca806178813c0dbce831dfe55
riya-dubey2535 <riyadubey2635@gmail.com>
2025-04-16 10:59:52 +0530

    adding logout

--- components/common/sidebar.php
+++ components/common/sidebar.php
@@ -1,10 +1,11 @@
 <!-- Sidebar -->
 <div class="flex">
 <?php
+// session_start();
 $loggedIn = isset($_SESSION['user']); // check if user is logged in
 ?>
 
-<aside class="w-64 bg-white h-screen p-5 shadow-lg">
+<aside class="w-64 bg-white h-screen p-5 shadow-lg relative">
   <div class="flex items-center space-x-2 text-xl font-bold mb-6">
     <img src="images/logo.png" class="w-8" />
     <span>CarRental</span>
@@ -13,15 +14,15 @@ $loggedIn = isset($_SESSION['user']); // check if user is logged in
   <nav class="mt-4 space-y-2">
     <?php if ($loggedIn): ?>
       <!-- Logged-in Sidebar -->
-      <a href="./dashboard" class="flex items-center p-3 text-blue-600 bg-gray-100 rounded-lg">
+      <a href="/dashboard" class="flex items-center p-3 text-blue-600 bg-gray-100 rounded-lg">
         <img src="https://cdn-icons-png.flaticon.com/512/9292/9292669.png" class="w-5 mr-2">
         Dashboard
       </a>
-      <a href="./components/Pages/booking.php" class="flex items-center p-3 text-gray-700 hover:bg-gray-200 rounded-lg">
+      <a href="/components/Pages/booking.php" class="flex items-center p-3 text-gray-700 hover:bg-gray-200 rounded-lg">
         <img src="https://cdn-icons-png.flaticon.com/512/747/747310.png" class="w-5 mr-2">
         Bookings
       </a>
-      <a href="./components/Pages/customer.php" class="flex items-center p-3 text-gray-700 hover:bg-gray-200 rounded-lg">
+      <a href="/components/Pages/customer.php" class="flex items-center p-3 text-gray-700 hover:bg-gray-200 rounded-lg">
         <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" class="w-5 mr-2">
         Customers
       </a>
@@ -29,7 +30,7 @@ $loggedIn = isset($_SESSION['user']); // check if user is logged in
         <img src="https://cdn-icons-png.flaticon.com/512/1170/1170678.png" class="w-5 mr-2">
         Payments
       </a>
-      <a href="./components/Pages/settings.php" class="flex items-center p-3 text-gray-700 hover:bg-gray-200 rounded-lg">
+      <a href="/components/Pages/settings.php" class="flex items-center p-3 text-gray-700 hover:bg-gray-200 rounded-lg">
         <img src="https://cdn-icons-png.flaticon.com/512/2099/2099058.png" class="w-5 mr-2">
         Settings
       </a>
@@ -53,4 +54,15 @@ $loggedIn = isset($_SESSION['user']); // check if user is logged in
       </a>
     <?php endif; ?>
   </nav>
+
+  <!-- Logout Button -->
+  <?php if ($loggedIn): ?>
+    <div class="absolute bottom-5 left-5 right-5">
+      <a href="./components/Pages/logout.php" class="flex items-center justify-center p-3 bg-red-100 text-red-600 rounded-lg hover:bg-red-200">
+        <img src="https://cdn-icons-png.flaticon.com/512/1828/1828479.png" class="w-5 mr-2">
+        Logout
+      </a>
+    </div>
+  <?php endif; ?>
 </aside>
+</div>
